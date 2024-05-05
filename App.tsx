00a4a0634c6ba712749bda8626d53393b79962c1
AatsuTakahashi <aatsu_t@icloud.com>
2024-05-06 03:00:12 +0900

    [add]: WorkOutPageを作成

--- App.tsx
+++ App.tsx
@@ -2,19 +2,25 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomePage from './src/components/pages/HomePage';
+import WorkOutPage from './src/components/pages/WorkOutPage';
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home'>
+      <Stack.Navigator initialRouteName='WorkOut'>
         <Stack.Screen
           name='Home'
           component={HomePage}
           options={{
             headerShown: false,
           }}
+        />
+        <Stack.Screen
+          name='WorkOut'
+          component={WorkOutPage}
+          options={{ headerShown: false }}
         />
       </Stack.Navigator>
     </NavigationContainer>
---
+++ src/components/pages/WorkOutPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import MainTemplate from '../templates/MainTemplate';
+import { Text, View } from 'react-native';
+
+const WorkOutPage = () => {
+  return (
+    <MainTemplate>
+      <View>
+        <Text>こんにちわ</Text>
+      </View>
+    </MainTemplate>
+  );
+};
+
+export default WorkOutPage;
